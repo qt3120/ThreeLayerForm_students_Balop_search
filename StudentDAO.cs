3c3fc99466fe4d3584d4709d79224f6fa654d034
qt3120 <66146935+qt3120@users.noreply.github.com>
2021-03-01 11:44:07 +0700

    DAL

---
+++ StudentDAO.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ThreeLayerForm.Core
+{
+    class StudentDAO
+    {
+        private DBConnection dbConnection = null;
+        public StudentDAO()
+        {
+            dbConnection = new DBConnection("localhost", "students");
+        }
+        public DataTable searchByName(string name)
+        {
+            string query = string.Format("select * from Students where Name like @_name");
+            SqlParameter[] sqlParameters = new SqlParameter[1];
+            sqlParameters[0] = new SqlParameter("@_name", SqlDbType.VarChar);
+            sqlParameters[0].Value = "%" + name + "%";
+            return dbConnection.executeSelectQuery(query, sqlParameters);
+        }
+        public DataTable searchById(string id)
+        {
+            string query = "select * from Students where ID = @_id";
+            SqlParameter[] sqlParameters = new SqlParameter[1];
+            sqlParameters[0] = new SqlParameter("@_id", SqlDbType.VarChar);
+            sqlParameters[0].Value = id;
+            return dbConnection.executeSelectQuery(query, sqlParameters);
+        }
+        public List<Students> getStudents() {
+            List<Students> list = new List<Students>();
+
+            return list;
+        }
+    }
+}
